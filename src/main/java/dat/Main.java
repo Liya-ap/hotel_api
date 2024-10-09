3298b29b50b23b137ec62688fe62a545346b880c
Liya-ap <144211578+Liya-ap@users.noreply.github.com>
2024-10-09 10:00:16 +0200

    populating of db data from main

--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -1,6 +1,7 @@
 package dat;
 
 import dat.config.ApplicationConfig;
+import dat.config.Populate;
 import dat.controllers.impl.ExceptionController;
 import dat.exceptions.ApiException;
 import dat.exceptions.Message;
@@ -13,6 +14,7 @@ import org.slf4j.LoggerFactory;
 public class Main {
 
     public static void main(String[] args) {
+        Populate.populateDatabase();
         ApplicationConfig.startServer(7070);
     }
 }
